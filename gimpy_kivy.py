e1b22097165370de7ecf2af3aae40c6974ca2238
martin <sideproject1892@gmail.com>
2021-05-09 17:30:57 +0100

    Arrow keys bound to allow navigation. Docstrings updated for all classes and methods

--- gimpy_kivy.py
+++ gimpy_kivy.py
@@ -25,6 +25,20 @@ from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 
 class MySaveDialog(Popup):
     def save_file(self, path):
+        """
+        Gets a file savename from the user
+
+        Parameters
+        ----------
+        path : string
+            directory to save the settings file
+
+        Returns
+        -------
+        savepath : string
+            full path for saving the setttings file as json
+
+        """
         savepath = os.path.join(path, self.ids.dialog_savename.text)
         if not savepath.endswith('.json'):
             savepath += '.json'
@@ -37,10 +51,16 @@ class ImageDirDialog(Popup):
     pass
 
 class SettingsScreen(Screen):
-    def __init__(self, **kwargs):
-        super(SettingsScreen, self).__init__(**kwargs)
-        
     def on_pre_enter(self):
+        """
+        Generates the layout for the settings screen
+        just before entering the screen
+
+        Returns
+        -------
+        None.
+
+        """
         Window.size = (400, 500)
         
         # add box layout to avoid issue with
@@ -74,101 +94,271 @@ class SettingsScreen(Screen):
         self.add_widget(overall)
         
     def open_save_dialog(self, instance):
+        """
+        Opens a popup to allow user to choose a save directory
+
+        Parameters
+        ----------
+        instance : MySaveDialog
+            instance of the MySaveDialog class
+
+        Returns
+        -------
+        None.
+
+        """
         saving = Factory.MySaveDialog()
         saving.open()
 
     def save_settings(self):
+        """
+        Save a dictionary of key-value pairs of numbers
+        and class labels to a json file
+
+        Returns
+        -------
+        None.
+
+        """
         d = dict((i, e.text) for i, e in enumerate(self.entries))
         with open(self.savepath, "w") as f:
             json.dump(d, f)
     
     def open_load_dialog(self, instance):
+        """
+        Opens a popup to allow user to choose a settings file
+        
+        Parameters
+        ----------
+        instance : LoadDialog
+            instance of the LoadDialog class
+
+        Returns
+        -------
+        None.
+
+        """
         loading = Factory.LoadDialog()
         loading.open()
         
     def load_settings(self, fname):
-        print("running load settings function")
-        
-        # load the data
+        """
+        Load settings from a user-selected json file
+
+        Parameters
+        ----------
+        fname : string
+            full path to the settings file
+
+        Returns
+        -------
+        None.
+
+        """
         with open(fname, 'r') as f:
             data = json.load(f)
         for entry, val in zip(self.entries, data.values()):
             entry.text = val
             
     def choose_image_dir(self, instance):
+        """
+        Opens a popup to allow user to choose a directory of images
+
+        Parameters
+        ----------
+        instance : ImageDirDialog
+            instance of the ImageDirDialog class
+
+        Returns
+        -------
+        None.
+
+        """
         choose = Factory.ImageDirDialog()
         choose.open()
     
-    def start_annotate(self, instance):
-        # bind classes to the key presses
-        for i, e in enumerate(self.entries):
-            pass
-
 class ViewerScreen(Screen):
     def __init__(self, **kwargs):
         super(ViewerScreen, self).__init__(**kwargs)
         self._keyboard = Window.request_keyboard(None, self)
         self._keyboard.bind(on_key_up=self._on_keyboard_up)
+        self.mapping = {'left' : self.nav_left, 'right' : self.nav_right,
+                       'up' : self.nav_up, 'down' : self.nav_down}
     
     def on_pre_enter(self):
+        """
+        Create the layout just before entering the screen
+
+        Returns
+        -------
+        None.
+
+        """
         Window.size = (700, 700)
         
         # get image filenames and info
         os.chdir(self.imgdir)
         self.imglist = sorted([i for i in os.listdir() if i.endswith('.tif')])
         self.img_idx = 0
+        self.num_images = len(self.imglist)
+        
+        # layout the screen
+        self.layout = BoxLayout(orientation="vertical")
+        self.layout_screen()
+        self.add_widget(self.layout)
+        
+    def layout_screen(self):
+        """
+        Layout the viewer with the filename and
+        an image of said file
+
+        Returns
+        -------
+        None.
+
+        """
         
         # prepare widgets
+        self.layout.clear_widgets()
         self.current = self.imglist[self.img_idx]
         self.image = imread(self.current)
-        fig = plt.Figure(figsize=(5,5))
-        self.ax = fig.add_subplot(111)
+        
+        # remove current_slice attribute if dealing with mix
+        # of multislice and single slice images
+        if hasattr(self, "current_slice"):
+            delattr(self, "current_slice")
+        
+        # plot the image with matplotlib
         try:
-            self.ax.imshow(self.image, cmap = 'viridis')
+            plt.imshow(self.image, cmap="viridis")
         except TypeError:
             self.current_slice = 0
             self.max_slice = self.image.shape[0] - 1
-            self.ax.imshow(self.image[self.current_slice], cmap='viridis')
-        img = imread(self.current)[self.img_idx]
-        plt.imshow(img, cmap="viridis")
+            plt.imshow(self.image[self.current_slice], cmap='viridis')
         plt.axis("off")
         plt.tight_layout()
         
-        # pack widgets into layout
-        layout = BoxLayout(orientation="vertical")
         self.imglbl = Label(text=f"{self.current}", size_hint_y=None,
                             height=60)
-        layout.add_widget(self.imglbl)
-        layout.add_widget(FigureCanvasKivyAgg(plt.gcf()))
-        self.add_widget(layout)
+        canvas = FigureCanvasKivyAgg(plt.gcf())
+        self.layout.add_widget(self.imglbl)
+        self.layout.add_widget(canvas)
+        
     
     def _on_keyboard_up(self, keyboard, keycode):
+        """
+        When keyboard button is released, map the button to a method
+
+        Parameters
+        ----------
+        keyboard : kivy.core.window.Keyboard
+            kivy class to access keyboard key presses
+        keycode : tuple
+            numeric code for the key and the text of said key
+
+        Returns
+        -------
+        None.
+
+        """
         text = keycode[1]
-        mapping = {'left' : self.nav_left, 'right' : self.nav_right,
-                   'up' : self.nav_up, 'down' : self.nav_down}
-        func = mapping[text]
-        func()
+        # ensure key is accounted for in mapping dictionary
+        if text in self.mapping.keys(): 
+            func = self.mapping[text]
+            func()
         
     def nav_left(self):
-        print('left')
+        """
+        Move left in the list of images (towards index 0)
+
+        Returns
+        -------
+        None.
+
+        """
+        if self.img_idx > 0:
+            self.img_idx -= 1
+            self.layout_screen()
     
     def nav_right(self):
-        print('right')
+        """
+        Move right in the list of images (towards last index)
+
+        Returns
+        -------
+        None.
+
+        """
+        if self.img_idx + 1 < self.num_images:
+            self.img_idx += 1
+            self.layout_screen()
     
     def nav_up(self):
-        print('up')
+        """
+        Navigate up a slice
+
+        Returns
+        -------
+        None.
+
+        """
+        self.update_slice(1)
     
     def nav_down(self):
-        print('down')
+        """
+        Navigate down a slice
+
+        Returns
+        -------
+        None.
+
+        """
+        self.update_slice(-1)
         
-    def update_slice(self):
-        pass
+    def update_slice(self, num):
+        """
+        Navigate between slices in a multislice images
+        and update the canvas
+
+        Parameters
+        ----------
+        num : integer
+            -1 or 1, informs whether to go up or down a slice
+
+        Returns
+        -------
+        None.
+
+        """
+        
+        # check if multisclice tiff
+        if hasattr(self, "current_slice"):
+            self.layout.remove_widget(self.layout.children[0])
+            plt.cla()
+            
+            # nav down
+            if num == -1 and self.current_slice > 0:
+                self.current_slice -= 1
+            elif num == 1 and self.current_slice + 1 < self.max_slice:
+                self.current_slice += 1
+            plt.imshow(self.image[self.current_slice], cmap='viridis')
+            plt.axis("off")
+            plt.tight_layout()
+            canvas = FigureCanvasKivyAgg(plt.gcf())
+            self.layout.add_widget(canvas)
     
-    def change_image(self):
-        pass
 
 class gimpyApp(App):
     def build(self):
+        """
+        Build method to create the app
+
+        Returns
+        -------
+        self.sm : screen manager instance
+            the screen manager
+
+        """
         self.sm = ScreenManager()
         self.sm.add_widget(SettingsScreen(name="settings"))
         self.sm.add_widget(ViewerScreen(name="viewer"))
